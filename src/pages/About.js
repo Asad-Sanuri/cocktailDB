fbe6294a67d2f2bfe78a3decb6084b91d99bc321
Asad-Sanuri <asad.sanuri@gmail.com>
2022-05-24 18:01:49 +0200

    about edit

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,9 +3,11 @@ import React from "react";
 export default function About() {
   return (
     <section className="section about-section">
-      <h1 className="section-title">about us</h1>
+      <h1 className="section-title">about</h1>
       <p>
         Project was made using the <b><a href="https://www.thecocktaildb.com/">TheCocktailDB.</a></b>
+
+        Search for your favorite cocktails to see the list of ingredients and how to make them.
       </p>
     </section>
   );
