c61b19f960a4a0341a487044d37cfbea0e06de74
Asad-Sanuri <asad.sanuri@gmail.com>
2022-05-24 21:18:03 +0200

    about edit

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,7 +5,7 @@ export default function About() {
     <section className="section about-section">
       <h1 className="section-title">about</h1>
       <p>
-        Project was made using the <b><a href="https://www.thecocktaildb.com/">TheCocktailDB.</a></b>
+        Project was made using the <b><a href="https://www.thecocktaildb.com/">TheCocktailDB.</a></b><br />
 
         Search for your favorite cocktails to see the list of ingredients and how to make them.
       </p>
